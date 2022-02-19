fcdcaf7c67dd08c832bd4ea42ba055b59f92a1e6
utkarshpant112 <utkarshpant112@gmail.com>
2022-02-19 14:56:30 -0800

    Login functionality

---
+++ frontend/src/components/Main.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import Login from "./Login/Login";
+import Navbar from "./LandingPage/Navbar";
+//Create a Main Component
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        {/*Render Different Component based on Route*/}
+        <Route path="/" component={Navbar} />
+        <Route path="/login" component={Login} />
+      </div>
+    );
+  }
+}
+//Export The Main Component
+export default Main;
