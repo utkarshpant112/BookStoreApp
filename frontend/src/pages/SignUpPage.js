eba5892100434019ad6305c72fb65814bc7dc819
utkarshpant112 <utkarshpant112@gmail.com>
2022-03-05 18:33:36 -0800

    Sign Up Page

---
+++ frontend/src/pages/SignUpPage.js
@@ -0,0 +1,118 @@
+import React, { Component } from "react";
+import axios from "axios";
+import cookie from "react-cookies";
+import { Navigate } from "react-router";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+
+//Define a Login Component
+class SignUpPage extends Component {
+  //call the constructor method
+  constructor(props) {
+    //Call the constrictor of Super class i.e The Component
+    super(props);
+    //maintain the state required for this component
+    this.state = {
+      username: "",
+      password: "",
+      authFlag: false,
+      message: undefined,
+    };
+    //Bind the handlers to this class
+    this.usernameChangeHandler = this.usernameChangeHandler.bind(this);
+    this.passwordChangeHandler = this.passwordChangeHandler.bind(this);
+    this.submitLogin = this.submitLogin.bind(this);
+  }
+  //Call the Will Mount to set the auth Flag to false
+  componentWillMount() {
+    this.setState({
+      authFlag: false,
+    });
+  }
+  //username change handler to update state variable with the text entered by the user
+  usernameChangeHandler = (e) => {
+    this.setState({
+      username: e.target.value,
+    });
+  };
+  //password change handler to update state variable with the text entered by the user
+  passwordChangeHandler = (e) => {
+    this.setState({
+      password: e.target.value,
+    });
+  };
+  //submit Login handler to send a request to the node backend
+  submitLogin = (e) => {
+    var headers = new Headers();
+    //prevent page from refresh
+    e.preventDefault();
+    const data = {
+      username: this.state.username,
+      password: this.state.password,
+    };
+    //set the with credentials to true
+    axios.defaults.withCredentials = true;
+    //make a post request with the user data
+    axios.post("http://localhost:3001/login", data).then((response) => {
+      console.log("Status Code : ", response.status);
+      if (response.status === 200 && response.data === "Successful Login") {
+        this.setState({
+          authFlag: true,
+        });
+      } else {
+        this.setState({
+          authFlag: false,
+          message: response.data,
+        });
+      }
+    });
+  };
+
+  render() {
+    let redirectVar = null;
+    if (cookie.load("cookie")) {
+      redirectVar = <Navigate to="/home" />;
+    }
+    return (
+      <div>
+        {redirectVar}
+        <div class="container">
+          <div class="login-form">
+            <div class="main-div">
+              <div class="panel">
+                <h2>Create Your Account</h2>
+                <p>Please enter your details</p>
+              </div>
+
+              <div class="form-group">
+                <input
+                  onChange={this.usernameChangeHandler}
+                  type="email"
+                  class="form-control"
+                  name="email"
+                  placeholder="email"
+                />
+              </div>
+              <div class="form-group">
+                <input
+                  onChange={this.passwordChangeHandler}
+                  type="password"
+                  class="form-control"
+                  name="password"
+                  placeholder="Password"
+                />
+              </div>
+              <button onClick={this.submitLogin} class="btn btn-primary">
+                Login
+              </button>
+              <div class={this.state.message ? "visible" : "invisible"}>
+                <div class="alert alert-danger">{this.state.message}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+//export Login Component
+export default SignUpPage;
